9cd9dbee6f09c0447e5d92354ed8f32f271268e1
supchyan <mikhailsupov@yandex.ru>
2025-11-24 18:17:21 +0300

    server log changes

--- tests/server/server.js
+++ tests/server/server.js
@@ -6,12 +6,12 @@ const
 app.use(bodyParser.text());
 
 app.post("/", (req, res) => {
-    console.log(req.body);
+    console.log(`ETHERNET COMMAND: ${req.body}`);
     res.send("TEST_ETH_RESPONSE");
 });
 
 app.post("/test", (req, res) => {
-    console.log(req.body);
+    console.log(`WLAN SECRET: ${req.body}`);
     res.send("TEST_WLAN_RESPONSE");
 });
 
